cc3e2e26cdfefa6385f116158c7eb8e10b5e723e
Jana Ismail <jana.ismail.nss@gmail.com>
2025-08-01 09:42:24 -0500

    Project dependencies | Test Snowflake connection | Set up main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import snowflake.connector
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
+@app.get("/health")
+async def health_check():
+    # Test connection to Snowflake
+    conn = snowflake.connector.connect(
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+        schema=os.getenv("SNOWFLAKE_SCHEMA_BRONZE")
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT CURRENT_VERSION()")
+    version = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return {"status": "healthy", "snowflake_version": version}
+
+
 @app.get("/")
-async def read_covid_data():
+async def filter_dataset():
     pass
